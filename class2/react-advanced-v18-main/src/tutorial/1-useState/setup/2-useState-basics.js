ea57cf2e4c58b18e4cea01c588e7638110cffd95
AndrejBesh <beslievski.a@gmail.com>
2022-11-21 21:37:41 +0100

    useState-Basics

--- class2/react-advanced-v18-main/src/tutorial/1-useState/setup/2-useState-basics.js
+++ class2/react-advanced-v18-main/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
-
+import React, { useState } from 'react'
+//useState - function
+// starts with use
+// component must be uppercase
+// invoke inside function/component body
+// don't call hooks conditonally
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
-};
+  const [text, setText] = useState('random title')
+  const changeTitleHandler = () => {
+    if (text === 'random title') {
+      setText('Hello world')
+    } else {
+      setText('random title')
+    }
+  }
+  return (
+    <>
+      <h1>{text}</h1>
+      <button type='button' className='btn' onClick={changeTitleHandler}>
+        Change Title
+      </button>
+    </>
+  )
+}
 
-export default UseStateBasics;
+export default UseStateBasics
